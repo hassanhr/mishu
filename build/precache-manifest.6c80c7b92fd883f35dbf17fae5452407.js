24b78f6e904751aab8a76a4043421a5e38daaf25
hassanhr <glucosemishu@gmail.com>
2018-12-05 05:46:26 +0600

    Create a React app and publish it to GitHub Pages

---
+++ build/precache-manifest.6c80c7b92fd883f35dbf17fae5452407.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb179c9a65dff4943929",
+    "url": "/mishu/static/js/runtime~main.cb179c9a.js"
+  },
+  {
+    "revision": "f00d3360ee67132fc038",
+    "url": "/mishu/static/js/main.f00d3360.chunk.js"
+  },
+  {
+    "revision": "d855c10c80aa95827f2a",
+    "url": "/mishu/static/js/1.d855c10c.chunk.js"
+  },
+  {
+    "revision": "f00d3360ee67132fc038",
+    "url": "/mishu/static/css/main.f43df2ca.chunk.css"
+  },
+  {
+    "revision": "fdb770d5daad511bad2c1080912f880f",
+    "url": "/mishu/index.html"
+  }
+];
